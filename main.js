bf143d66babef160be3a04042571e15c032c5228
HardCoreWare <bernardocodu@gmail.com>
2018-12-19 18:03:01 -0600

    listo

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ let gauss = new Gauss();
 gauss.init([5,5,5,5,7,7,7]);
 gauss.update();
 let size=gauss.getSize();
+let average=gauss.getAverage();
 
-console.log('Size : '+String(size));
+console.log('Size : ' + String(size));
+console.log('Average : ' + String(size));
